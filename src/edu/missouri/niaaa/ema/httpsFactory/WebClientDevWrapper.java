b71c0e1ef3282816eeb0ba61839ef6ec15933846
Ricky <rs79c@mail.missouri.edu>
2014-10-26 23:25:59 -0500

    ema_stl

---
+++ src/edu/missouri/niaaa/ema/httpsFactory/WebClientDevWrapper.java
@@ -0,0 +1,32 @@
+package edu.missouri.niaaa.ema.httpsFactory;
+
+import org.apache.http.HttpVersion;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
+
+import android.content.Context;
+
+public class WebClientDevWrapper extends DefaultHttpClient {
+	public static HttpClient getSpecialKeyStoreClient(Context context) {
+		BasicHttpParams params = new BasicHttpParams();
+		HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+		HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+		HttpProtocolParams.setUseExpectContinue(params, true);
+
+		SchemeRegistry schReg = new SchemeRegistry();
+		schReg.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+		schReg.register(new Scheme("https", CustomerSocketFactory.getSocketFactory(context), 443));
+
+		ClientConnectionManager connMgr = new ThreadSafeClientConnManager(params, schReg);
+
+		return new DefaultHttpClient(connMgr, params);
+	}
+}
